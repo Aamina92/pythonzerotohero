914e4b2874b2cd7597ce66f277f0fcac95dd99ca
Maryam <56219307+Aamina92@users.noreply.github.com>
2024-12-18 13:14:11 +0530

    Create variables.py
    Python Variables

---
+++ variables.py
@@ -0,0 +1,18 @@
+# Day 1 : 17 Dec 2024 - Python Variable example 
+# Assigning different types of values to variables
+
+name = "Tuba"            # String variable
+age = 2					 # Integer variable
+height = 1.1             # Float variable
+is_student = False       # Boolean variable
+
+# printing the variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is student:", is_student)
+
+# Performing operations using variables
+new_age = age + 1 # incremeneting age by 1
+print("Next year age will be :", new_age)
+
